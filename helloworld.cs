e05fa0a57f76f44902652b56fbcb12dac8afb7e5
PanaMiguel12 <miguelruizbezana2010@gmail.com>
2025-01-10 17:29:14 +0100

    primera version 0.1
    Hoy he saludado al mundo

---
+++ helloworld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+/*Hola buenas tardes
+Buenos días te gustaría participar en una pequeña encuesta
+Sí
+Vale, dígame cual es su top 99 de mejores videojuegos*/
+print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
